463ee975d58eb0cabf090e7d98ae5626d890520a
Felix Geller <fgeller@gmail.com>
2011-09-16 00:20:59 +0200

    Reducing the code

--- problem_030/solution.py
+++ problem_030/solution.py
@@ -3,20 +3,16 @@ import time
 # upper limit: 6 * pow(9,5) < 999 999
 
 def run():
-    exp = 5
-    finds = []
-    sums = []
+    exp, finds, sums = 5, [], []
     for ndigs in range(0, 6):
         newsums = []
         for num in range(1, 10):
-            numsum = pow(num, exp)
-            curnum = num * pow(10, ndigs)
-            newsums.append((curnum, numsum))
-            for restnum, restsum in sums:
-                newnum = curnum + restnum
-                newsum = numsum + restsum
-                newsums.append((newnum, newsum))
-                if newnum == newsum: finds.append(newnum)
+            nsum, cnum = pow(num, exp), num * pow(10, ndigs)
+            newsums.append((cnum, nsum))
+            ntpls = [(cnum + rnm, nsum + rsm) for rnm, rsm in sums]
+            newsums.extend(ntpls)
+            for nnm, nsm in ntpls:
+                if nnm == nsm: finds.append(nnm)
         sums.extend(newsums)
 
     print "Found %s for exp %s. Their sum is %s." % (finds, exp, sum(finds))
